8c713bf0549f4389f19d6c1c3f0473cacf6a03f8
HaiDinh2020 <haidinh6008@gmail.com>
2023-01-02 21:32:09 +0700

    tạo file khaitu, dinhchinh, thanhviencuaho, tieusu

---
+++ src/main/java/com/example/nhom16_cnpm/thanh_vien_cua_ho.java
@@ -0,0 +1,27 @@
+package com.example.nhom16_cnpm;
+
+public class thanh_vien_cua_ho {
+	private int idNhanhKhau;
+	private int idHoKhau;
+	private String quanHeVoiChuHo;
+	
+	public int getIdNhanhKhau() {
+		return idNhanhKhau;
+	}
+	public void setIdNhanhKhau(int idNhanhKhau) {
+		this.idNhanhKhau = idNhanhKhau;
+	}
+	public int getIdHoKhau() {
+		return idHoKhau;
+	}
+	public void setIdHoKhau(int idHoKhau) {
+		this.idHoKhau = idHoKhau;
+	}
+	public String getQuanHeVoiChuHo() {
+		return quanHeVoiChuHo;
+	}
+	public void setQuanHeVoiChuHo(String quanHeVoiChuHo) {
+		this.quanHeVoiChuHo = quanHeVoiChuHo;
+	}
+	
+}
