6e68bcf73d61964fc819f3242b6d3de0afa1fd84
hieu1911 <109023520+hieu1911@users.noreply.github.com>
2023-02-12 17:06:52 +0700

    khoi phuc lai ho khau + nhan khau

--- src/main/java/com/example/controller/HoKhauManageController/ChonChuHoController.java
+++ src/main/java/com/example/controller/HoKhauManageController/ChonChuHoController.java
@@ -4,9 +4,12 @@ package com.example.controller.HoKhauManageController;
 
 import java.io.IOException;
 
+
 import java.net.URL;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.ResourceBundle;
 
@@ -18,86 +21,90 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
-public class ChonChuHoController extends ThemMoiHoKhauController implements Initializable {
-	
-	private Stage stage;
-	private Scene scene;
-	private Parent root;
-	
-    @FXML
-    private TextField chonChuHoTf;
-    @FXML
-    private TextField chuHoTf;
-    @FXML
-    private Button xacNhanBt;
-    @FXML
-    private Button huyBoBt;
-    @FXML
-    private TableView<NhanKhau> chonChuHoTv;
-    @FXML
-    private TableColumn<NhanKhau, String> diaChiHienNayCol;
-    @FXML
-    private TableColumn<NhanKhau, String> gioiTinhCol;
-    @FXML
-    private TableColumn<NhanKhau, String> hoTenCol;
-    @FXML
-    private TableColumn<NhanKhau, Date> ngaySinhCol;
-    @FXML
-    private TableColumn<NhanKhau, String> soCMTCol;
-    private ObservableList<NhanKhau> chuHoList;
-    
-    private String hoTen ;
-    private Date ngaySinh;
-    private String soCMT;
-    
+public class ChonChuHoController  implements Initializable {
+
+
+	@FXML
+	private TextField chonChuHoTf;
+	@FXML
+	private TextField chuHoTf;
+	@FXML
+	private Button xacNhanBt;
+	@FXML
+	private Button huyBoBt;
+	@FXML
+	private TableView<NhanKhau> chonChuHoTv;
+	@FXML
+	private TableColumn<NhanKhau, String> diaChiHienNayCol;
+	@FXML
+	private TableColumn<NhanKhau, String> gioiTinhCol;
+	@FXML
+	private TableColumn<NhanKhau, String> hoTenCol;
+	@FXML
+	private TableColumn<NhanKhau, Date> ngaySinhCol;
+	@FXML
+	private TableColumn<NhanKhau, String> soCMTCol;
+	private ObservableList<NhanKhau> chuHoList;
+
+	private int idChuHo;
+	private String hoTen ;
+	private Date ngaySinh;
+	private String soCMT;
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		
+
 		chuHoList = FXCollections.observableArrayList(
-				
+
 		);
-    	hoTenCol.setCellValueFactory(new PropertyValueFactory<NhanKhau, String>("hoTen"));
-    	ngaySinhCol.setCellValueFactory(new PropertyValueFactory<NhanKhau, Date>("namSinh"));
-    	gioiTinhCol.setCellValueFactory(new PropertyValueFactory<NhanKhau, String>("gioiTinh"));
-    	diaChiHienNayCol.setCellValueFactory(new PropertyValueFactory<NhanKhau, String>("diaChiHienNay"));
-    	soCMTCol.setCellValueFactory(new PropertyValueFactory<NhanKhau, String>("soCMT"));;
-    	chonChuHoTv.setItems(chuHoList);
+		hoTenCol.setCellValueFactory(new PropertyValueFactory<NhanKhau, String>("hoTen"));
+		ngaySinhCol.setCellValueFactory(new PropertyValueFactory<NhanKhau, Date>("namSinh"));
+		gioiTinhCol.setCellValueFactory(new PropertyValueFactory<NhanKhau, String>("gioiTinh"));
+		diaChiHienNayCol.setCellValueFactory(new PropertyValueFactory<NhanKhau, String>("diaChiHienNay"));
+		soCMTCol.setCellValueFactory(new PropertyValueFactory<NhanKhau, String>("soCMT"));;
+		chonChuHoTv.setItems(chuHoList);
 		showInfor();
-		
+
 		chonChuHoTv.setOnMouseClicked(new EventHandler<MouseEvent>(){
 
-            @Override
-            public void handle(MouseEvent event) {
-				 int id = chonChuHoTv.getSelectionModel().getSelectedItem().getID();
-				 
-				 NhanKhauService conn = new NhanKhauService();
-				 if (conn.checkPerson(id)) {
-					 hoTen = chonChuHoTv.getSelectionModel().getSelectedItem().getHoTen();
-					 soCMT = chonChuHoTv.getSelectionModel().getSelectedItem().getSoCMT();
-					 ngaySinh = chonChuHoTv.getSelectionModel().getSelectedItem().getNamSinh();
-					 chuHoTf.setText(hoTen);
-				 } else {
-					 // hiện thông báo không chọn được
-					 chuHoTf.setText("");
-				 }
+			@Override
+			public void handle(MouseEvent event) {
+				idChuHo = chonChuHoTv.getSelectionModel().getSelectedItem().getID();
+
+				NhanKhauService conn = new NhanKhauService();
+				if (conn.checkPerson(idChuHo)) {
+					hoTen = chonChuHoTv.getSelectionModel().getSelectedItem().getHoTen();
+					soCMT = chonChuHoTv.getSelectionModel().getSelectedItem().getSoCMT();
+					ngaySinh = chonChuHoTv.getSelectionModel().getSelectedItem().getNamSinh();
+					chuHoTf.setText(hoTen);
+				} else {
+					chuHoTf.setText("");
+					Alert alert = new Alert(AlertType.WARNING);
+					alert.setTitle("Warning Dialog");
+					alert.setContentText("Nhân khẩu đã nằm trong hộ mới");
+					alert.showAndWait();
+				}
 			}
 		});
 	}
-	    
-    private void showInfor() {
-    	NhanKhauService conn = new NhanKhauService();
+
+	private void showInfor() {
+		NhanKhauService conn = new NhanKhauService();
 		ResultSet rs = conn.chonChuHoSelect();
 		try {
 			if (rs != null) {
@@ -116,50 +123,37 @@ public class ChonChuHoController extends ThemMoiHoKhauController implements Init
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 	}
-    
-    
-    
-    @FXML
-    void huyBo(ActionEvent event) {
-    	huyBoBt.getScene().getWindow().hide();
-    }
-    
-    @FXML
-    void xacNhan(ActionEvent event) throws IOException {
-//    	FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/hokhaumanage/them-moi-ho-khau.fxml"));	
-//		root = loader.load();	
-//    	ThemMoiHoKhauController themHoKhau = loader.getController();
-//    	themHoKhau.setThongTinChuHo(hoTen, ngaySinh, soCMT);
-//        Node node = (Node) event.getSource();
-//        stage = (Stage) node.getScene().getWindow();
-//		scene = new Scene(root);
-//		stage.setScene(scene);
-//		stage.show();
-    	
-    	System.out.println("ngay sinh la " + ngaySinh);
-    	Node node = (Node) event.getSource();
-    	Stage stage = (Stage) node.getScene().getWindow();
-    	
-//    	DataHoKhauMoi.hoTenChuHo = hoTen;
-//    	super.setChuHoTf(hoTen);
-    	stage.close();
-//    	System.out.println("dong nay duoc doc sau khi close stage");
-//    	super.setChuHoTf(hoTen);
-//    	soCMTTf.setText(soCMT);
-//    	super.setSoCMTTf(soCMT);
-    	
-//    	ThemMoiHoKhauController tm = new ThemMoiHoKhauController();
-//    	tm.setThongTinChuHo(hoTen, ngaySinh, soCMT);
-//    	stage.setUserData(tm);
-    	
-    	
-    	
-    }
 
-    
-    
-    
-    
+	@FXML
+	void huyBo(ActionEvent event) {
+		huyBoBt.getScene().getWindow().hide();
+	}
+
+	@FXML
+	void xacNhan(ActionEvent event) throws IOException {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/hokhaumanage/them-moi-ho-khau.fxml"));
+		Parent root = loader.load();
+		ThemMoiHoKhauController themHoKhau = loader.getController();
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd");
+		String strDate = dateFormat.format(ngaySinh);
+		DataHoKhauMoi.hoTenChuHo = hoTen;
+		DataHoKhauMoi.ngaySinh = strDate;
+		DataHoKhauMoi.soCMT = soCMT;
+		DataHoKhauMoi.idChuHo = idChuHo;
+		themHoKhau.setThongTinChuHo();
+
+		Node node = (Node) event.getSource();
+		Stage stage = (Stage) node.getScene().getWindow();
+		stage.getOwner().hide();
+		Scene scene = new Scene(root);
+		stage.setTitle("Thêm mới hộ khẩu");
+		stage.setScene(scene);
+		stage.show();
+	}
+
+
+
+
+
 }
--- src/main/java/com/example/controller/HoKhauManageController/ThemMoiHoKhauController.java
+++ src/main/java/com/example/controller/HoKhauManageController/ThemMoiHoKhauController.java
@@ -3,14 +3,13 @@ package com.example.controller.HoKhauManageController;
 import java.io.IOException;
 
 
+
 import java.net.URL;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.Date;
 import java.util.ResourceBundle;
 
-import com.example.controller.Controller;
 import com.example.services.HoKhauService;
+import com.example.main.QuanLyNhanKhau;
 import com.example.model.ThanhVienCuaHo;
 
 import javafx.collections.FXCollections;
@@ -22,6 +21,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -31,145 +31,132 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
-public class ThemMoiHoKhauController extends Controller implements Initializable {
+public class ThemMoiHoKhauController  implements Initializable {
 
-    @FXML
-    private Button chonChuHoBt;
-    @FXML
-    private Button chonThanhVienBt;
-    @FXML
-    private TextField chuHoTf;
-    @FXML
-    private TextField diaChiTf;
-    @FXML
-    private TextField maHoKhauTf;
-    @FXML
-    private TextField ngaySinhChuHoTf;
-    @FXML
-    protected TextField soCMTTf;
-    @FXML
-    private Button huyBoBt;
-    @FXML
-    private Button taoBt;
-    @FXML
-    private TextField maKhuVucTf;
-    @FXML
-    private TableView<ThanhVienCuaHo> thanhVienTv;
-    @FXML
-    private TableColumn<ThanhVienCuaHo, String> hoTenCol;
-    @FXML
-    private TableColumn<ThanhVienCuaHo, Date> ngaySinhCol;
-    @FXML
-    private TableColumn<ThanhVienCuaHo, String> quanHeCol;
-    private ObservableList<ThanhVienCuaHo> thanhVienList;
-    
-    
-    @Override
+	@FXML
+	private Button chonChuHoBt;
+	@FXML
+	private Button chonThanhVienBt;
+	@FXML
+	private TextField chuHoTf;
+	@FXML
+	private TextField diaChiTf;
+	@FXML
+	private TextField maHoKhauTf;
+	@FXML
+	private TextField maKhuVucTf;
+	@FXML
+	private TextField ngaySinhChuHoTf;
+	@FXML
+	protected TextField soCMTTf;
+	@FXML
+	private Button huyBoBt;
+	@FXML
+	private Button taoBt;
+
+	@FXML
+	private TableView<ThanhVienCuaHo> thanhVienTv;
+	@FXML
+	private TableColumn<ThanhVienCuaHo, String> hoTenCol;
+	@FXML
+	private TableColumn<ThanhVienCuaHo, Date> ngaySinhCol;
+	@FXML
+	private TableColumn<ThanhVienCuaHo, String> quanHeCol;
+	private ObservableList<ThanhVienCuaHo> thanhVienList;
+
+
+	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-    	thanhVienList = FXCollections.observableArrayList(
-				
+		thanhVienList = FXCollections.observableArrayList(
+
 		);
-    	hoTenCol.setCellValueFactory(new PropertyValueFactory<ThanhVienCuaHo, String>("hoTen"));
-    	ngaySinhCol.setCellValueFactory(new PropertyValueFactory<ThanhVienCuaHo, Date>("namSinh"));
-    	quanHeCol.setCellValueFactory(new PropertyValueFactory<ThanhVienCuaHo, String>("quanHeVoiChuHo"));
-    	thanhVienTv.setItems(thanhVienList);
-	}
-    
-    private void showInfor() {
-    	HoKhauService conn = new HoKhauService();
-		ResultSet rs = conn.getThanhVien();
-		try {
-			if (rs != null) {
-				while (rs.next()) {
-					ThanhVienCuaHo tvch = new ThanhVienCuaHo();
-					tvch.setHoTen(rs.getString("hoTen"));
-					tvch.setNamSinh(rs.getDate("namSinh"));
-					tvch.setQuanHeVoiChuHo(rs.getString("quanHeVoiChuHo"));
-					thanhVienList.add(tvch);
-				}
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+		hoTenCol.setCellValueFactory(new PropertyValueFactory<ThanhVienCuaHo, String>("hoTen"));
+		ngaySinhCol.setCellValueFactory(new PropertyValueFactory<ThanhVienCuaHo, Date>("namSinh"));
+		quanHeCol.setCellValueFactory(new PropertyValueFactory<ThanhVienCuaHo, String>("quanHeVoiChuHo"));
+		thanhVienTv.setItems(thanhVienList);
 	}
 
 	@FXML
-    void chonChuHo(ActionEvent event) throws IOException {
-		
+	void chonChuHo(ActionEvent event) throws IOException {
+		luuThongTin();
 		Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-    	FXMLLoader loader = new FXMLLoader();			
-		loader.setLocation(getClass().getResource("/com/example/hokhaumanage/chon-chu-ho.fxml"));			
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("/com/example/hokhaumanage/chon-chu-ho.fxml"));
 		loader.load();
 		Parent root = loader.getRoot();
 		Stage modal_dialog = new Stage(StageStyle.DECORATED);
-        modal_dialog.initModality(Modality.WINDOW_MODAL);
-        modal_dialog.initOwner(stage);
-        modal_dialog.setTitle("Chọn chủ hộ");
-        Scene scene = new Scene(root);	
+		modal_dialog.initModality(Modality.WINDOW_MODAL);
+		modal_dialog.initOwner(stage);
+		modal_dialog.setTitle("Chọn chủ hộ");
+		Scene scene = new Scene(root);
 		modal_dialog.setScene(scene);
 		modal_dialog.show();
-    }
-	
-	public void setThongTinChuHo(String hoTen, Date ngaySinh, String soCMT) { 
-		chuHoTf.setText(hoTen);
-//		LocalDate date = ngaySinh.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-//		ngaySinhChuHoDpk.setValue(date);
-		soCMTTf.setText(soCMT);
-	}
-
-    @FXML
-    void chonThanhVien(ActionEvent event) throws IOException {
-    	Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-    	FXMLLoader loader = new FXMLLoader();			
-		loader.setLocation(getClass().getResource("/com/example/hokhaumanage/chon-thanh-vien.fxml"));			
-		loader.load();
-		Parent root = loader.getRoot();
-		Stage modal_dialog = new Stage(StageStyle.DECORATED);
-        modal_dialog.initModality(Modality.WINDOW_MODAL);
-        modal_dialog.initOwner(stage);
-        modal_dialog.setTitle("Chọn thành viên");
-        Scene scene = new Scene(root);	
-		modal_dialog.setScene(scene);
-		modal_dialog.show();
-    }
-
-    
-    
-    public String getChuHoTf() {
-		return chuHoTf.getText();
-	}
-
-	public void setChuHoTf(String chuHo) {
-		chuHoTf.setText("sao khong đổi");
-		System.out.println("dong nay chay trong them moi controller");
-	}
-
-	
-
-	public void setNgaySinhChuHoTf(Date ngaySinhChuHo) {
-		ngaySinhChuHoTf.setText(ngaySinhChuHo.toString());
-	}
-
-	public String getSoCMTTf() {
-		return soCMTTf.getText();
-	}
-
-	public void setSoCMTTf(String soCMT) {
-		soCMTTf.setText("so cmt da duoc đổi");
 	}
 
 	@FXML
-    void huyBo(ActionEvent event) throws IOException {
-    	huyBoBt.getScene().getWindow().hide();
-    }
+	void chonThanhVien(ActionEvent event) throws IOException {
+		luuThongTin();
+		Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("/com/example/hokhaumanage/chon-thanh-vien.fxml"));
+		loader.load();
+		Parent root = loader.getRoot();
+		Stage modal_dialog = new Stage(StageStyle.DECORATED);
+		modal_dialog.initModality(Modality.WINDOW_MODAL);
+		modal_dialog.initOwner(stage);
+		modal_dialog.setTitle("Chọn thành viên");
+		Scene scene = new Scene(root);
+		modal_dialog.setScene(scene);
+		modal_dialog.show();
+	}
 
-    @FXML
-    void tao(ActionEvent event) {
+	public void luuThongTin() {
+		DataHoKhauMoi.hoTenChuHo = chuHoTf.getText();
+		DataHoKhauMoi.ngaySinh = ngaySinhChuHoTf.getText();
+		DataHoKhauMoi.soCMT = soCMTTf.getText();
+		DataHoKhauMoi.diaChi = diaChiTf.getText();
+		DataHoKhauMoi.maHoKhau = maHoKhauTf.getText();
+		DataHoKhauMoi.maKhuVuc = maKhuVucTf.getText();
+		DataHoKhauMoi.thanhVienCuaHoList = thanhVienList;
+	}
 
-    }
+	public void setThongTinChuHo() {
+		chuHoTf.setText(DataHoKhauMoi.hoTenChuHo);
+		ngaySinhChuHoTf.setText(DataHoKhauMoi.ngaySinh);
+		soCMTTf.setText(DataHoKhauMoi.soCMT);
+		diaChiTf.setText(DataHoKhauMoi.diaChi);
+		maHoKhauTf.setText(DataHoKhauMoi.maHoKhau);
+		maKhuVucTf.setText(DataHoKhauMoi.maKhuVuc);
+		thanhVienList.addAll(DataHoKhauMoi.thanhVienCuaHoList);
+	}
 
-	
+	@FXML
+	void huyBo(ActionEvent event) throws IOException {
+		huyBoBt.getScene().getWindow().hide();
+	}
+
+	@FXML
+	void tao(ActionEvent event) throws IOException {
+		if (chuHoTf.getText().trim().isEmpty() || diaChiTf.getText().trim().isEmpty()
+				|| maHoKhauTf.getText().trim().isEmpty()|| ngaySinhChuHoTf.getText().trim().isEmpty()
+				|| maKhuVucTf.getText().trim().isEmpty()|| soCMTTf.getText().trim().isEmpty()
+				|| thanhVienList.isEmpty()) {
+			Alert alert = new Alert(Alert.AlertType.WARNING);
+			alert.setTitle("cảnh báo");
+			alert.setContentText("Vui lòng nhập hết các trường bắt buộc");
+			alert.showAndWait();
+		} else {
+			HoKhauService hoKhau = new HoKhauService();
+			hoKhau.insertHoKhauMoi();
+			taoBt.getScene().getWindow().hide();
+			FXMLLoader fxmlLoader = new FXMLLoader(QuanLyNhanKhau.class.getResource("ho-khau.fxml"));
+			Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
+			QuanLyNhanKhau.window.setScene(scene);
+
+		}
+
+	}
+
+
 
 }
